91cec92702138fabc304a2ab6c8f2f5484f23cb7
Luiz Felipe <luizfeliperomero10@gmail.com>
2022-05-05 21:29:21 -0300

    Add header

--- src/app/player/components/index.ts
+++ src/app/player/components/index.ts
@@ -1,3 +1,4 @@
 export * from './play';
 export * from './sidebar';
 export * from './group';
+export * from './navbar';
--- src/app/player/player.module.ts
+++ src/app/player/player.module.ts
@@ -1,13 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-import { PlayComponent, GroupComponent, SidebarComponent } from './components';
-import { SoundService } from './services';
 import { HttpClientModule } from '@angular/common/http';
 
+import {
+  PlayComponent,
+  GroupComponent,
+  SidebarComponent,
+  NavbarComponent,
+} from './components';
+import { SoundService } from './services';
+
 @NgModule({
-  declarations: [PlayComponent, GroupComponent, SidebarComponent],
+  declarations: [
+    PlayComponent,
+    GroupComponent,
+    SidebarComponent,
+    NavbarComponent,
+  ],
   imports: [CommonModule, HttpClientModule],
   providers: [SoundService],
+  exports: [PlayComponent, GroupComponent, SidebarComponent, NavbarComponent],
 })
 export class PlayerModule {}
